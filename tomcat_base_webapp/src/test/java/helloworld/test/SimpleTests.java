c5cdc449cb1558f988b6e4820d9494f44d61480f
Michael Lehmann <michael.2.lehmann@mailbox.org>
2020-06-20 16:30:58 +0200

    Added Junit Test

---
+++ tomcat_base_webapp/src/test/java/helloworld/test/SimpleTests.java
@@ -0,0 +1,16 @@
+package helloworld.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTests {
+
+	@Test
+	@DisplayName("1 + 1 = 2")
+	void addsTwoNumbers() {
+		int result = 1+1;
+		assertEquals(2, result, "1 + 1 should equal 2");
+	}
+}
